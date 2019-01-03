a50ff5daa1f499277f4035dd4d775a929335b1eb
Oxana Bykova <bykova.oksana@gmail.com>
2019-01-03 11:15:08 -0500

    page technical diving

---
+++ technical-diving/price-technical.tsx
@@ -0,0 +1,81 @@
+interface TechDivingPrice {
+    title: string,
+    subtitle: string,
+    price: string
+}
+
+const priceListTechnicalDaily: TechDivingPrice[] = [
+    {
+        title: 'день технического дайвинга',
+        subtitle: 'одно декомпрессионное погружение в день + рекреационное погружение на остатках газа',
+        price: '150 $'
+    }
+]
+
+const priceListTechnicalRent: TechDivingPrice[] = [
+    {
+        title: 'техническое крыло',
+        subtitle: ' в сборе со спинкой',
+        price: '20 $'
+    },
+    {
+        title: 'BCD сайдмаунт',
+        subtitle: 'X-Deep Stealth 2.0',
+        price: '30 $'
+    },
+    {
+        title: 'комплект регуляторов',
+        subtitle: 'под спарку или сайдмаунт',
+        price: '30 $'
+    },
+    {
+        title: 'стейджевый регулятор',
+        subtitle: 'с банджой',
+        price: '12 $'
+    },
+    {
+        title: 'O2 ощищенный регулятор',
+        subtitle: '',
+        price: '12 $'
+    },
+    {
+        title: 'Подводный скутер',
+        subtitle: '',
+        price: '70 $'
+    },
+    {
+        title: 'Гелий',
+        subtitle: 'за литр',
+        price: '0,07 €'
+    },
+    {
+        title: 'Кислород EANx 100%',
+        subtitle: '6 литровый баллон',
+        price: '20 $'
+    },
+    {
+        title: 'Найтрокс 50%',
+        subtitle: '12 литровый стейдж',
+        price: '20 $'
+    },
+    {
+        title: 'Найтрокс 40%',
+        subtitle: '12 литровый стейдж',
+        price: '15 $'
+    },
+    {
+        title: 'Найтрокс 36%',
+        subtitle: '12 литровый / 15 литровый баллон',
+        price: '8 $ /10 $'
+    },
+    {
+        title: 'Найтрокс 32%',
+        subtitle: '12 литровый / 15 литровый баллон',
+        price: '6 $ / 8 $'
+    },
+    {
+        title: '',
+        subtitle: '',
+        price: ''
+    }
+]
