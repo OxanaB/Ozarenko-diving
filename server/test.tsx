31092f882734eeab49a481b816eeb9be50cba836
Oxana Bykova <bykova.oksana@gmail.com>
2019-01-04 15:49:32 -0500

    technical diving section done

--- server/test.tsx
+++ server/test.tsx
@@ -7,44 +7,37 @@ import { TechdivingCoursesNdl } from '../technical-diving/techdiving-courses-ndl
 import { Rebreather } from '../technical-diving/rebreather-component'
 import { writeFileSync } from 'fs'
 import { Page } from '../technical-diving/page';
-import { metaTechnicalDivingPage, metaSidemountPage } from '../technical-diving/meta-technical-diving';
+import { metaTechnicalDivingPage, metaSidemountPage, metaTechDivingRebreather, metaTechCoursesIantd, metaTechCoursesNdl } from '../technical-diving/meta-technical-diving';
 
-const pages: any = [{
+
+const pages: any = {
     './technical-diving/technical-diving.html': ReactDom.renderToString(
         <Page meta={metaTechnicalDivingPage} title={'Технический дайвинг с Виктором Озаренко на Черном и на Красном море'}>
             <TechnicalDiving />
         </Page>
-    )
-},
-{
+    ),
     './technical-diving/sidemount.html': ReactDom.renderToString(
         <Page meta={metaSidemountPage} title={'Обучение дайвингу в сайдмаунте (sidemount diving)'}>
             <Sidemount />
         </Page>
-    )
-},
-{
+    ),
     './technical-diving/techdiving-courses-iantd.html': ReactDom.renderToString(
-        <Page meta={metaSidemountPage} title={'Курсы технического дайвинга IANTD'}>
+        <Page meta={metaTechCoursesIantd} title={'Курсы технического дайвинга IANTD'}>
             <TechdivingCoursesIantd />
         </Page>
-    )
-},
-{
+    ),
     './technical-diving/techdiving-courses-ndl.html': ReactDom.renderToString(
-        <Page meta={metaSidemountPage} title={'Курсы технического дайвинга NDL'}>
+        <Page meta={metaTechCoursesNdl} title={'Курсы технического дайвинга NDL'}>
             <TechdivingCoursesNdl />
         </Page>
-    )
-},
-{
+    ),
     './technical-diving/rebreather-diving.html': ReactDom.renderToString(
-        <Page meta={metaSidemountPage} title={'Ребризер дайвинг на Красном море, обучение. Megaladon & Inspiration'}>
+        <Page meta={metaTechDivingRebreather} title={'Ребризер дайвинг на Красном море, обучение. Megaladon & Inspiration'}>
             <Rebreather />
         </Page>
     )
-}
-]
+};
+
 for (const name in pages) {
     const html = pages[name];
     writeFileSync(name, html, { encoding: 'utf-8' });
