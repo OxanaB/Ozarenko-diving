1e6fedc73107d9dc5a6368a6962c45798a7c806a
Oxana Bykova <bykova.oksana@gmail.com>
2019-01-03 22:22:22 -0500

    html for tech dive section

--- technical-diving/meta-technical-diving.tsx
+++ technical-diving/meta-technical-diving.tsx
@@ -5,23 +5,7 @@ export interface Meta {
 }
 
 export const metaTechnicalDivingPage: Meta[] = [
-    {
-        name: "fb_admins_meta_tag",
-        content: "viktor.ozarenko"
-    },
-    {
-        property: "og:type",
-        content: "website"
-    },
-    {
-        property: "fb:admins",
-        content: "viktor.ozarenko" 
-    },
-    {
-        property:"og:site_name",
-        content:"Школа дайвинга Виктора Озаренко. Филиалы в Одессе и Шарм-эль-Шейхе"
-    },
-    {
+        {
         name: "description",
         content:"Технические погружения на найтроксе, воздухе, тримиксе в компании подводного диверсанта самый безопасный вид технодайвинга. Всегда можно взять сайдмаунт"
     },
@@ -30,7 +14,7 @@ export const metaTechnicalDivingPage: Meta[] = [
         content:"Приглашаем на курсы технического дайвинга в Одессе. Всю линейку курсов Advanced nitrox, Technical diver, Normoxic trimix, Trimix diver проводит мастер инструктор тренер Виктор Озаренко. Курсы можно пройти в конфигурации сайдмаунт"
     },
     {
-        property:"og:title", 
+        property:"og:title",
         content:"Технический дайвинг с Виктором Озаренко на Черном и на Красном море"
     },
     {
@@ -40,5 +24,97 @@ export const metaTechnicalDivingPage: Meta[] = [
     {
         property:"og:description",
         content:"Технические погружения на найтроксе, воздухе, тримиксе в компании подводного диверсанта самый безопасный вид технодайвинга. Всегда можно взять сайдмаунт"
+    }
+]
+
+export const metaSidemountPage: Meta[] = [
+    {
+        name: "description",
+        content: "Сайдмаунт - ваши первые шаги в технический дайвинг! Удобная конфигурация позволяет разгрузить спину и повысить безопасность ваших погружений"
+    },
+    {
+        name: "keywords",
+        content: "Приглашаем на курсы обучения сайдмаунту в Одессе и Шарм эль Шейхе. Попробуйте удобный и легкий стиль подводного плавания в sidemount. Всю линейку курсов технического дайвинга можно пройти в сайдмаунте. Курсы проводит инструктор тренер Виктор Озаренко."
+    },
+    {
+        property: "og:title",
+        content: "Обучение дайвингу в сайдмаунте (sidemount diving)"
+    },
+    {
+        property: "og:url",
+        content: "https://www.ozarenko-diving.com/technical-diving/sidemount.html"
+    },
+    {
+        property: "og:description",
+        content: "Сайдмаунт - ваши первые шаги в технический дайвинг! Удобная конфигурация позволяет разгрузить спину и повысить безопасность ваших погружений"
+    }
+]
+
+export const metaTechCoursesIantd: Meta[] = [
+    {
+        name: "description",
+        content: "Курсы технического дайвинга по системе IANTD - классика глубоких погружений."
+    },
+    {
+        name: "keywords",
+        content: "Приглашаем на курсы технического дайвинга в Одессе. Всю линейку курсов Advanced nitrox, Technical diver, Normoxic trimix, Trimix diver проводит инструктор тренер Виктор Озаренко. Курсы можно пройти в конфигурации сайдмаунт."
+    },
+    {
+        property: "og:title",
+        content: "Курсы технического дайвинга IANTD"
+    },
+    {
+        property: "og:url",
+        content: "https://www.ozarenko-diving.com/technical-diving/techdiving-courses-iantd.html"
+    },
+    {
+        property: "og:description",
+        content: "Курсы технического дайвинга по системе IANTD - классика глубоких погружений."
+    }
+]
+
+export const metaTechCoursesNdl: Meta[] = [
+    {
+        name: "description",
+        content: "Авторская программа подготовки технических дайверов Виктор Озаренко"
+    },
+    {
+        name: "keywords",
+        content: "Приглашаем на курсы технического дайвинга в Одессе. Всю линейку курсов технического дивизиона НДЛ проводит автор программы мастер IDC TECH инструктор Виктор Озаренко. Курсы можно пройти в конфигурации сайдмаунт."
+    },
+    {
+        property: "og:title",
+        content: "Курсы технического дайвинга NDL"
+    },
+    {
+        property: "og:url",
+        content: "https://www.ozarenko-diving.com/technical-diving/techdiving-courses-ndl.html"
+    },
+    {
+        property: "og:description",
+        content: "Авторская программа подготовки технических дайверов Виктор Озаренко"
+    }
+]
+
+export const metaTechDivingRebreather: Meta[] = [
+    {
+        name: "description",
+        content: "Прогружения и обучение дайвингу на ребризерах Megalodon и Inspiration в Шарм эль Шейхе, Египет."
+    },
+    {
+        name: "keywords",
+        content: "ребризер, обучение на ребризере, ребризер египет, megaladon, inspiration, мегаладон, инспирейшн"
+    },
+    {
+        property: "og:title",
+        content: "Ребризер дайвинг на Красном море, обучение. Megaladon & Inspiration"
+    },
+    {
+        property: "og:url",
+        content: "https://www.ozarenko-diving.com/technical-diving/rebreather-diving.html"
+    },
+    {
+        property: "og:description",
+        content: "Прогружения и обучение дайвингу на ребризерах Megalodon и Inspiration в Шарм эль Шейхе, Египет."
     }
 ]
---
+++ technical-diving/rebreather-price.tsx
@@ -0,0 +1,139 @@
+export interface RebreatherCourses {
+    title: string;
+    subtitle: string;
+    description: string;
+    price: string    
+}
+
+export const rebreatherPricelistCourses: RebreatherCourses[] = [
+    {
+        title: 'Discover Rebreather',
+        subtitle: 'один день, два погружения, максимальная глубина 15 метров',
+        description: 'Попробовать погрузиться на ребризере может любой дайвер уровня не ниже Open water diver начиная с 15 лет. За время программы Discover Rebreather вы изучите основную концепцию погружений на аппаратах с замкнутым циклом дыхания, отработаете базовые упражнения в закрытой воде и совершите погружение-экскурсию на ребризере на глубину до 15 метров.',
+        price: '160 €'
+    },
+    {
+        title: 'Megalodon full rebreather diver',
+        subtitle: '5-6 дней, 10 погружений, максимальная глубина 42 м + 15 минут deco',
+        description: 'На курс принимаются студенты, имеющие уровень Advanced nitrox diver и минимум 50 погружений (с 18 лет). Это начальный полный курс обучения на ребрезире, на котором вы получаете базовые навыки и знания погружений на аппарате с замкнутым циклом дыхания модели Megalоdon, для совершения технических погружений на глубины до 42 метров с максимальной декомпрессией 15 минут. ',
+        price: '1200 €'
+    },
+    {
+        title: 'Megalodon advanced Nitrox diver',
+        subtitle: '6-7 дней, 12 погружений, максимальная глубина 42 м + 15 минут deco',
+        description: 'Этот курс предназначен для тех дайверов, которые решили начать свой путь в мир технического дайвинга с погружений на ребризере. Курс сочетает в себе базовый курс технического дайвинга Advanced nitrox diver, а также навыки и теорию по дайвингу на ребризере модели Megalоdon. Минимальные требования по зачислению на курс: уровень Advanced open water diver и Nitrox diver, минимальное количество погружений в лог-буке 50, возраст 18 лет. ',
+        price: '1650 €'
+    },
+    {
+        title: 'CCR normoxic trimix diver',
+        subtitle: '5 дней, 5 погружений, максимальная глубина 60 м',
+        description: 'После того как вы стали ребризер-дайвером и наныряли 25 часов на аппарате с замкнутым циклом дыхания, вы можете перейти на следующий уровнь - погружения до 60 метров. Этот курс - аналог Normoxic trimix diver на открытом цикле, но с использованием ребризера. Если вы уже являетесь Normoxic trimix дайвером, вам будет намного проще адаптироваться под глубокие погружения на нормокстических смесях при погружении на ребризере.',
+        price: '1050 €'
+    },
+    {
+        title: 'CCR hypoxic trimix diver',
+        subtitle: '5 дней, 5 погружений, максимальная глубина 100 м',
+        description: 'Третий уровень в обучении дайвингу на ребризере. Минимальные требования к студенту: наличие сертификата CCR normoxic trimix diver, опыт 50 часов дайвинга на ребризере, возраст 18 лет.',
+        price: '1300 €'
+    },
+    {
+        title: 'Кроссовер на модель Megalodon',
+        subtitle: '3 дня, 4 погружения',
+        description: 'Если вы уже являетесь ребризер дайвером, но на другой модели ребризера, и хотите изучить особенности погружений на ребризере типа Megaladon, тогда этот курс для вас.',
+        price: '550 €'
+    }
+]
+
+export interface RebreatherDaily {
+    days: string;
+    price: string
+}
+
+export const rebreatherPricelistAir: RebreatherDaily[] = [
+    {
+        days: 'один день',
+        price: '80 €'
+    },
+    {
+        days: 'два дня',
+        price: '160 €'
+    },
+    {
+        days: 'три дня',
+        price: '235 €'
+    },
+    {
+        days: 'четыре дня',
+        price: '310 €'
+    },
+    {
+        days: 'пять дней',
+        price: '380€'
+    }
+]
+export const rebreatherPricelistDecoPro: RebreatherDaily[] = [
+    {
+        days: 'один день',
+        price: '90 €'
+    },
+    {
+        days: 'два дня',
+        price: '180 €'
+    },
+    {
+        days: 'три дня',
+        price: '260 €'
+    },
+    {
+        days: 'четыре дня',
+        price: '345 €'
+    },
+    {
+        days: 'пять дней',
+        price: '425€'
+    }
+]
+export const rebreatherPricelistNormoxic: RebreatherDaily[] = [
+    {
+        days: 'один день',
+        price: '100 €'
+    },
+    {
+        days: 'два дня',
+        price: '200 €'
+    },
+    {
+        days: 'три дня',
+        price: '295 €'
+    },
+    {
+        days: 'четыре дня',
+        price: '390 €'
+    },
+    {
+        days: 'пять дней',
+        price: '485€'
+    }
+]
+export const rebreatherPricelistHypoxic: RebreatherDaily[] = [
+    {
+        days: 'один день',
+        price: '120 €'
+    },
+    {
+        days: 'два дня',
+        price: '240 €'
+    },
+    {
+        days: 'три дня',
+        price: '350 €'
+    },
+    {
+        days: 'четыре дня',
+        price: '470 €'
+    },
+    {
+        days: 'пять дней',
+        price: '580€'
+    }
+]
