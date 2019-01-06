59da95105f547e1bc18a545970aa38b3d0550d70
Oxana Bykova <bykova.oksana@gmail.com>
2019-01-05 22:38:41 -0500

    styles for main page

--- main-page-component.tsx
+++ main-page-component.tsx
@@ -4,33 +4,33 @@ import { map } from './utils';
 
 export class MainPageComponent extends React.Component {
     render() {
-        return <div className='page'>
-            <div className='slider-main-page'>
+        return <div className="page">
+            <div className="slider-main-page">
 
             </div>
 
-            <div className='main-page-link-elements'>
+            <div className="main-page-link-elements">
                 {map(mainPageLinkElements, (elements) => {
-                    return <div className='main-page-link-element'>
-                        <div className='main-page-link-element-text'>
+                    return <div className="main-page-link-element">
+                        <div className="main-page-link-element-image">
+                            <a href={elements.buttonLink}>
+                                <img src={elements.image} alt={elements.imageAlt} />
+                            </a>
+                        </div>
+                        <div className="main-page-link-element-text">
                             <h2>{elements.title}</h2>
                             <h3>{elements.subtitle}</h3>
                             <h3>{elements.price}</h3>
                             <p>{elements.description}</p>
-                            <button className='button-go-to-page'>
-                                <a href={elements.buttonLink}>{elements.buttonText}</a>
-                            </button>
-                        </div>
-                        <div className='main-page-link-element-image'>
-                            <a href={elements.buttonLink}>
-                                <img src={elements.image} alt={elements.imageAlt} width='260px' height='260px'/>
-                            </a>
+                            <a
+                                className="button-go-to-page"
+                                href={elements.buttonLink}>{elements.buttonText}</a>
                         </div>
                     </div>
                 })}
             </div>
 
-            <div className='news-feed'>
+            <div className="news-feed">
 
             </div>
         </div>
