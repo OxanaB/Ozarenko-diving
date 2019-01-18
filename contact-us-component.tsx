80aee6716b5f356fe1a53a776167740e12202355
Oxana Bykova <bykova.oksana@gmail.com>
2019-01-18 10:33:35 -0500

    styles contac us and booking form

--- contact-us-component.tsx
+++ contact-us-component.tsx
@@ -4,11 +4,18 @@ import { enableClient } from './client';
 export class ContactUs extends React.Component {
     render() {
         return <div className="page-container">
-            <h1>Контакты в Одессе</h1>
+            <div className="wrapper-two-columns">
+                <div><h1>Контакты в Одессе</h1>
 
             <p>E-mail <a href="mailto:ozarenko.diving@gmail.com">ozarenko.diving@gmail.com</a></p>
 
             <p>Тел. +38 068 498 1046 (Виктор)</p>
+                </div>
+            <div>
+            <h1>Оставить заявку на дайвинг</h1>
+            <iframe className="booking" src="https://goo.gl/forms/lyhFZT7DxAwRLBZH2" width="640" height="1158" />
+                </div>
+                </div>
         </ div>
     }
 }
