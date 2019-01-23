3f858b8190b7db0188f589ed9431e9409bcf3991
Oxana Bykova <bykova.oksana@gmail.com>
2019-01-22 21:45:45 -0500

    mobile styling header aand menu button

--- header-component.tsx
+++ header-component.tsx
@@ -3,17 +3,11 @@ import * as React from 'react';
 export class Header extends React.Component {
     render() {
         return <div className="header">
-            <div className="header-container">
+            
                 <a href="/index.html" ></a> 
-                <div className="header-text-elements">
-                    <p>Школа дайвинга Виктора Озаренко, курсы обучения дайвингу PADI, IANTD, NDL</p>
-                    <a href="mailto:ozarenko.diving@gmail.com">ozarenko.diving@gmail.com</a>
-                </div>
-            </div>
+                <p>Школа дайвинга Виктора Озаренко, курсы обучения дайвингу PADI, IANTD, NDL</p>
+                <a href="mailto:ozarenko.diving@gmail.com">ozarenko.diving@gmail.com</a>
+            
         </div>
     }
 }
-
-{/* <img className="header-img"  
-                    src="/images/logos/ozarenko_logo-main.png"
-                    alt="Школа дайвинга Виктора Озаренко. Дайвинг клуб в Одессе: курсы PADI, NDL, IANTD. Техничсекий дайвинг. Фридайвинг" />*/}
--- menu-mobile-checker.tsx
+++ menu-mobile-checker.tsx
@@ -41,10 +41,7 @@ export class MenuMobileChecker extends React.Component<MenuProps, MenuMobileChek
                     this.setState({ isMenuOpened: !this.state.isMenuOpened })
                 }}>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" viewBox="0 0 42 42">
-                        <path fill="none" d="M0 0h24v24H0V0z" />
-                        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
-                    </svg>
+                    <div className="mobile-button"></div>
 
                 </div>;
             case 'desktop': return <Menu {...this.props} />;
